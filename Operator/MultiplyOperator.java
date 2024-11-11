2f746b5e878402fda8da40808d56680356fc1992
Krishna <know1singh@gmail.com>
2024-11-11 10:26:29 +0530

    first commit

---
+++ Operator/MultiplyOperator.java
@@ -0,0 +1,43 @@
+// Day-11
+// Chapter Operators
+// Multiply operator
+// Set -1
+/*
+class MultiplyOperator{
+ public static void main(String args[]){
+     int val1 = 10;
+	 int val2 = 20;
+	System.out.println("Calculate the value of A and B " + (val1*val2));
+ }
+}
+*/
+// Set -2
+/*
+class MultiplyOperator{
+	public static void main(String args[]){
+		System.out.println("A * B " + (10*10));
+	}
+}
+*/
+// Set -3
+/*
+class MultiplyOperator{
+	public static void main(String agrs[]){
+		System.out.println("Value of a and b is " + (20*20));
+	}
+}
+*/
+// Set -4
+/*
+class MultiplyOperator{
+	public static void main(String args[]){
+		System.out.println("Calculate the Value of A and B " + (40*40));
+	}
+}
+*/
+//Set -5
+class MultiplyOperator{
+	public static void main(String args[]){
+		System.out.println("Values " + (10*20));
+	}
+}
