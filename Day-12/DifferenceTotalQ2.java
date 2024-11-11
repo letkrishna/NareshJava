d73956b03eb378faf67be9e578244165d4b1be32
Krishna <know1singh@gmail.com>
2024-11-11 22:11:32 +0530

    Day12 Q2

---
+++ Day-12/DifferenceTotalQ2.java
@@ -0,0 +1,12 @@
+// Day-12
+// Chapter Operators
+// Set -1
+//Q2. WAP to store any two integers and calculate and display their difference.
+class DifferenceTotalQ2{
+	public static void main(String args[]){
+		int num1 = 20;
+		int num2 = 10;
+		int differenceValue = num1-num2;
+		System.out.println("Difference value is " + (differenceValue));
+	}
+}
